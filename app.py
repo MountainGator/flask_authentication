ae665aeeaece66dd402573237553c100c2fa8ca3
MountainGator <luketaylorsanders@gmail.com>
2022-07-12 06:02:50 -0500

    something changed

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "urmom420"
-CORS(app, resources={r"/*": {"origins": "localhost:3000"}})
+CORS(app)
 
 client = MongoClient('localhost', 27017)
 db = client.feedback_db
@@ -36,36 +36,27 @@ def log_in():
     json = request.json
 
     u = collection.find_one({'username': json.username})
-
     if u and bcrypt.check_password_hash(u['password'], json.password):
         user = u['username']
         session['current_user'] = user
         return (user, 201)
-    else: return (jsonify(msg='Not Found'), 303)
+    else: return ('Not Found', 303)
 
 @app.route("/check", methods=["GET"])
 def find_current_user():
-    
-    if session['current_user']:
-        user = collection.find_one({'username': session['current_user']})
-        
-        if user['first_name']:
-            print('user', user)
-            user_obj = {
-                'username': user['username'],
-                'email': user['email'],
-                'first': user['first_name'],
-                'last': user['last_name']
-            }
-            response = jsonify(msg=user_obj)
-            response.headers.add('Access-Control-Allow-Origin', '*')
-            return (response, 201)
+    name = session['current_user']
+    if name: 
+        user = collection.find({'username': name})
+        user_obj = {
+            'username': user['username'],
+            'email': user['email'],
+            'first': user['first_name'],
+            'last': user['last_name']
+        }
+        if user:
+            return (jsonify(msg=user_obj), 201)
         else:
-            response = jsonify(msg='Not Found')
-            response.headers.add('Access-Control-Allow-Origin', '*')
-            return (response, 303)
+            return (jsonify(msg='Not Found'), 303)
     else:
         print('no user')
-        response = jsonify(msg='Not Found')
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return (response, 303)
+        return (jsonify(msg='Not Found'), 303)
